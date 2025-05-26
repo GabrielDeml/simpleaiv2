40ed6a052a75e761feffef7ed12afdcda1c1d475
GabrielDeml <gabrielddeml@gmail.com>
2025-05-26 12:55:46 -0400

    Update base path in svelte.config.js and add GitHub Pages deployment workflow
    - Changed the base path for production from '/simpleai' to '/simpleaiv2' in svelte.config.js.
    - Introduced a new GitHub Actions workflow for deploying the application to GitHub Pages, including build and deployment steps.

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/simpleai' : ''
+			base: process.env.NODE_ENV === 'production' ? '/simpleaiv2' : ''
 		}
 	}
 };
